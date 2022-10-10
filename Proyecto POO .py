d96f9577f061c87b2e8c60f00d418d3c7b284387
Pedro Pablo Guzmán Mayen <guz22111@uvg.edu.gt>
2022-10-09 21:35:23 -0600

    Implementación del método de preguntas
    Se implementó la primera versión del método que desplega preguntas a azar

--- Proyecto POO .py
+++ Proyecto POO .py
@@ -1,3 +1,5 @@
+import pandas as pd
+import random
 class user():
     pass
 
@@ -8,10 +10,35 @@ class tutor(user):
     pass
 
 class logic():
-    pass
+    condition = True
+    def game(self, preguntas):
+        #questions = preguntas["Pregunta"].unique
+        #answers = preguntas["Respuesta"].unique
+        
+        while self.condition:
+            question1 = random.choice(preguntas["Pregunta"])
+            print("Pregunta: " + question1)
+            print()
+            answer1 = input("Ingrese su respuesta: ")
+            
+            if answer1 not in preguntas["Respuesta"].unique():
+                print("Incorrecto")
+                self.condition = False
+            else:
+            
+                for i in range(len(preguntas)):
+                    if preguntas.loc[i]["Pregunta"] == question1 and answer1 == preguntas.loc[i]["Respuesta"]:
+                        print("Correcto")
+                
+                    
 
 class main():
     pass
 
-class databse():
-    pass
+class database():
+    questionslvl1 = pd.read_csv("preguntas_preprimaria.csv")
+
+l = logic()
+d = database()
+
+l.game(d.questionslvl1)
