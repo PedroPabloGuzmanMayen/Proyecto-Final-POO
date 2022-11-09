68984701d82d51bd6d142a75bf8eb4f971f0f6cc
Pedro Pablo Guzmán Mayen <guz22111@uvg.edu.gt>
2022-11-09 16:27:30 -0600

    Mejoras para entrega 4
    Detalles finales para la entrega 4

--- QuestionGUI.py
+++ QuestionGUI.py
@@ -1,7 +1,20 @@
 from tkinter import*
+import random
+import pandas as pd
 class questions:
     def __init__(self, user):
         self.window = Tk()
+        if (self.us.getscore() <= 500):
+            self.questions = 2
+        if ( 500 < self.us.getscore() <= 1000):
+            self.questions = 1
+        if (1000 < self.us.getscore() <= 1500):
+            self.questions = 3
+        if (1500 < self.us.getscore() <=2000):
+            self.questions = 4
+        self.question = random.randint(0,100)
+        self.answer = StringVar()
+        
         self.window.title("Juego de preguntas")
         self.us = user
         self.window.geometry("925x500+300+200")
@@ -9,4 +22,5 @@ class questions:
         self.window.mainloop()
         print(self.us.getusername())
         self.us.setscore(self.us.getscore()+20)
+
         print(self.us.getscore())
--- prueba.py
+++ prueba.py
@@ -1,3 +1,6 @@
+import random 
+import pandas as pd
+'''
 import pandas as pd
 import random
 from tkinter import*
@@ -15,3 +18,33 @@ class hello:
         self.button.pack()
         self.window.mainloop()
 hello()
+'''
+
+list = [1,2,3,4]
+
+random.shuffle(list)
+print(list)
+random.shuffle(list)
+print(list)
+random.shuffle(list)
+print(list)
+random.shuffle(list)
+print(list)
+print(list)
+random.shuffle(list)
+print(list)
+print(list)
+random.shuffle(list)
+print(list)
+
+hola = pd.read_excel("Questions.xlsx")
+
+print(hola["Option1"])
+
+adios = 50
+
+hola.at[4, "Option1"] = adios
+
+tankev = hola["Option1"].sample()
+
+hola.to_excel("Resources.xlsx", index = False)
