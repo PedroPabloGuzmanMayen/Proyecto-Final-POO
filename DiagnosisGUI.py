07d08f375748c377d20d47e3e9274415a8d52626
Pedro Pablo Guzmán Mayen <guz22111@uvg.edu.gt>
2022-11-25 16:38:59 -0600

    Método del exámen diagnóstico completamente implementado
    Se implemento por completo el método del examen diagnóstico

--- DiagnosisGUI.py
+++ DiagnosisGUI.py
@@ -7,24 +7,27 @@ from AddData import AddData
 class diagnosis:
 
     def nextButton(self):
-        self.numb +=1
-        if (self.numb >len(self.questions) -1):
+        self.counter +=1
+        if (self.counter >len(self.questions) -1):
             messagebox.showerror(message= "Terminaste el examen", title="Juego terminado")
         else:
             if (str(self.answer.get()) == str(self.questions.iloc[self.numb]["Answer"])):
                 messagebox.showerror(message= "Respuesta correcta", title="Respuesta correcta")
                 self.add.addScore(self.us.getusername(), 30)
-                diagnosis(self.us, self.numb)
+
             else:
                 messagebox.showerror(message= "Respuesta incorrecta", title="Respuesta inccorrecta")
-                diagnosis(self.us, self.numb)
+            self.window.destroy()
+            diagnosis(self.us, self.counter)
+            
 
     def __init__(self, user, number):
         self.window = Tk()
         self.us = user
         self.add = AddData()
         self.questions = pd.read_excel("Questions.xlsx")
-        self.numb = number
+        self.counter = number
+        self.numb = self.counter
         self.question_list = []
         self.question_list.append(self.questions.iloc[self.numb]["Answer"])
         self.question_list.append(self.questions.iloc[self.numb]["Option1"])
@@ -42,14 +45,7 @@ class diagnosis:
         for i in range(len(self.question_list)):
             self.radiob = Radiobutton(self.window, text = self.question_list[i], variable = self.answer, value = str(self.question_list[i]), command=self.nextButton)
             self.radiob.pack()
-        #self.nextbutton = Button(self.window, text="Siguiente", command= self.nextButton)
-        #self.nextbutton.place(x=425, y =300)
         self.window.geometry("925x500+300+200")
         self.window.configure(bg = "#fff")
         self.window.mainloop()
 
-us = user()
-
-us.setusername("dalas")
-us.setscore(20)
-diagnosis(us, 0)
